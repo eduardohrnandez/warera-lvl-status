6a5a5d7613da6a82cb7e57e7ae58d0e43faa1c56
eduardohrnandez <eduardojoseh21@gmail.com>
2026-02-26 12:26:32 -0400

    bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class MenuNiveles(discord.ui.Select):
             "Nivel 23": "https://media.discordapp.net/attachments/1475960551615103152/1475978734535184577/IMG-20260224-WA0025.jpg?ex=699f73d0&is=699e2250&hm=6222a4f8fc1fc0903ff1d6dcc69361bdd3216d24cc57efe06dcf4b920b257798&=&format=webp&width=606&height=268",
             "Nivel 24": "https://media.discordapp.net/attachments/1475960551615103152/1475979840837521625/IMG-20260224-WA0028.jpg?ex=699f74d8&is=699e2358&hm=7f945e20b372da2320a29e2101a71a7f8fdb3b98ec5db25973cdcd4771ff30d4&=&format=webp&width=602&height=274",
             "Nivel 25": "https://media.discordapp.net/attachments/1475960551615103152/1475983824398848061/IMG-20260224-WA0029.jpg?ex=699f788e&is=699e270e&hm=ce6805533856c409138ad59c169da16f2c782a02b54c83963e28598bf71bb430&=&format=webp&width=643&height=414",
-            "Nivel 26": "https://dummyimage.com/600x400/2ecc71/ffffff&text=ECO+Lvl+26",
+            "Nivel 26": "https://media.discordapp.net/attachments/1475960551615103152/1476281254453448917/IMG-20260225-WA0019.jpg?ex=69a1364f&is=699fe4cf&hm=73072c3aa42d9394b94e31852d2c27831d23bbd50f4f6aa041b8fd8518864047&=&format=webp&width=624&height=266",
             "Nivel 27": "https://dummyimage.com/600x400/2ecc71/ffffff&text=ECO+Lvl+27",
             "Nivel 28": "https://dummyimage.com/600x400/2ecc71/ffffff&text=ECO+Lvl+28",
             "Nivel 29": "https://dummyimage.com/600x400/2ecc71/ffffff&text=ECO+Lvl+29",
@@ -87,8 +87,8 @@ class MenuNiveles(discord.ui.Select):
             "Nivel 22": "https://media.discordapp.net/attachments/1475960551615103152/1475975979733221438/IMG-20260224-WA0023.jpg?ex=699f713f&is=699e1fbf&hm=b20268714abd197a29a52ecef6ad96aa4ba225518d6369b05ce89db50916cf26&=&format=webp&width=610&height=651",
             "Nivel 23": "https://media.discordapp.net/attachments/1475960551615103152/1475978734086656111/IMG-20260224-WA0026.jpg?ex=699f73d0&is=699e2250&hm=6538aa8a36ff5bfba30cbdec6d86ad04b58e3d7d3fc22b47f2805602ce3c89d6&=&format=webp&width=608&height=646",
             "Nivel 24": "https://media.discordapp.net/attachments/1475960551615103152/1475979841366134917/IMG-20260224-WA0027.jpg?ex=699f74d8&is=699e2358&hm=ad1f5886c7655d53e0fbd6b88f30d0a5e9bab3d48e02c82ebf5fabd28a8b5158&=&format=webp&width=598&height=648",
-            "Nivel 25": "https://media.discordapp.net/attachments/1475960551615103152/1475983823887011931/IMG-20260224-WA0030.jpg?ex=699f788e&is=699e270e&hm=9014067da796603f33b31a389b2faa59dcea4a380b8a7c44f66e3b6d13dd025b&=&format=webp&width=596&height=650",
-            "Nivel 26": "https://dummyimage.com/600x400/e74c3c/ffffff&text=WAR+Lvl+26",
+            "Nivel 25": "https://media.discordapp.net/attachments/1475960551615103152/1476280137023623329/IMG-20260225-WA0017.jpg?ex=69a13544&is=699fe3c4&hm=73c7851023e8fca4e150656c0932104eafa978d23a19ca5437312c1b791d369f&=&format=webp&width=606&height=661",
+            "Nivel 26": "https://media.discordapp.net/attachments/1475960551615103152/1476281255023611965/IMG-20260225-WA0018.jpg?ex=69a1364f&is=699fe4cf&hm=fd2ef327a7994857873e15f43f56724abe58b384451eaa1a01707051ea3e3d2e&=&format=webp&width=607&height=643",
             "Nivel 27": "https://dummyimage.com/600x400/e74c3c/ffffff&text=WAR+Lvl+27",
             "Nivel 28": "https://dummyimage.com/600x400/e74c3c/ffffff&text=WAR+Lvl+28",
             "Nivel 29": "https://dummyimage.com/600x400/e74c3c/ffffff&text=WAR+Lvl+29",
@@ -166,4 +166,5 @@ async def on_ready():
 
 mantener_vivo()
 bot.run(TOKEN)
+
 
